72fa4817436ad78587a6a083a9e8cc7ffec68685
javitocor <javitocor@gmail.com>
2020-04-19 16:50:46 +0200

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase! }
     validates :name, presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                 format: { with: VALID_EMAIL_REGEX },
                 uniqueness: true
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,5 +19,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
