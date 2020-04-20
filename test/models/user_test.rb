34f5bf663af5272aea21c9a80b2c6ce07846c78c
javitocor <javitocor@gmail.com>
2020-04-20 07:08:32 +0200

    Fixed problems with mail validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -34,7 +34,7 @@ class UserTest < ActiveSupport::TestCase
   end
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-    foo@bar_baz.com foo@bar+baz.com]
+    foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
     @user.email = invalid_address
     assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -42,7 +42,7 @@ class UserTest < ActiveSupport::TestCase
   end
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.save
+    @user.save
     assert_not duplicate_user.valid?
   end
   test "email addresses should be saved as lower-case" do
