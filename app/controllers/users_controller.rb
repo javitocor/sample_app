9a4ffe379a538dc2045fe0709f6added4ca4b1d7
javitocor <javitocor@gmail.com>
2020-04-26 18:40:13 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :delete]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -39,6 +40,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   private
 
@@ -58,4 +64,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 User.create!(name: "Example User",
     email: "example@railstutorial.org",
     password: "foobar",
-    password_confirmation: "foobar")
+    password_confirmation: "foobar", admin: true)
     # Generate a bunch of additional users.
     99.times do |n|
     name = Faker::Name.name
